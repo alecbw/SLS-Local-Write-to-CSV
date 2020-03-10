875238b57d1d23bc959e73361f26a6d0a04deffa
alecbw <abarrett.wilsdon@gmail.com>
2020-03-10 13:08:21 -0700

    finished cleanup

--- invoke_sls_local.py
+++ invoke_sls_local.py
@@ -1,8 +1,10 @@
+
+# Note: not used at present; simpler to just a bash script.
+
 from subprocess import call
 
 function = "lookup-website"
 data = '{"Search_Query":"gainsight"}'
 rc = call(["./invoke_sls_local.sh", function, data])
 print(rc)
-print('something else')
-#chmod u+rx invoke_sls_local.sh
+
