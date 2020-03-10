922c31b502639b751c67fe7ddedc8dbcc1d0fa92
alecbw <abarrett.wilsdon@gmail.com>
2020-03-10 13:03:07 -0700

    added the most recently working version. Adding a TODO log next

--- invoke_sls_local.sh
+++ invoke_sls_local.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
-echo "This far"
-echo "$1"
-echo "$2"
-OUTPUT="$(sls invoke local "$*")"
-echo "This far2"
 
-echo "${OUTPUT}"
-echo "this far 3 "
+INVOCATION=$(sls invoke local -f "$1" -d "$2")
+
+OUTPUT=$(echo "${INVOCATION}" | grep "data" | cut -d ":" -f 2- | sed 's/,$//'| jq -r '.' >> filename.csv)
+# echo $OUTPUT
+echo "Wrote to file, named: sls_invoke_file.csv"
+
+# if [ "$3" == "" ]
+# then
+#     "$OUTPUT" >> sls_invoke_file.csv
+#     echo "Wrote to file, named: sls_invoke_file.csv"
+# else
+#     "$OUTPUT" >> "$3"
+#     echo "Wrote to file, named: $3"
+
+# fi
+# echo $(echo "${OUTPUT}" | awk '{print $2}')
+
 # sls invoke local -f "$1" -d "$2"
+# OUTPUT="$(sls invoke local "$*")"
+# OUTPUT=$(sls invoke local "$*")
+# OUTPUT=$(sls invoke local -f "$1" -d "$2" | tail -1)
+# OUTPUT=$(sls invoke local -f "$1" -d "$2" | jq '.data')
+
+# OUTPUT=$(sls invoke local -f "$1" -d "$2" | awk "data")
+# var=$(echo $TEST|awk '{print $3}')
+# OUTPUT=$(echo "${INVOCATION}" | grep "data" | cut -d ":" -f 3- | echo >> "${3:-sls_invoke_file.csv}")
